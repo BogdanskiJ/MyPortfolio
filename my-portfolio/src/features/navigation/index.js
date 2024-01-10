fa336cc809fb69359e65ba3b85b3d5440782ca1f
Jakub Bogdanski <bogdanskikuba@gmail.com>
2024-01-10 14:04:35 +0100

    navigation added

--- my-portfolio/src/features/navigation/index.js
+++ my-portfolio/src/features/navigation/index.js
@@ -1,12 +1,38 @@
-import { StyledNav, StyledNavBox, StyledNavButton } from "./styled";
+import { StyledNav, StyledNavBox, StyledNavLi, StyledNavUl } from "./styled";
+
+const handleScroll = (e) => {
+	e.preventDefault();
+	const target = e.target.getAttribute("href");
+	const location = document.querySelector(target).offsetTop;
+
+	window.scrollTo({
+		left: 0,
+		top: location,
+		behavior: "smooth",
+	});
+};
 
 export const Navigation = () => {
 	return (
 		<StyledNavBox>
 			<StyledNav>
-				<StyledNavButton> Home</StyledNavButton>
-				<StyledNavButton> asdasfas</StyledNavButton>
-				<StyledNavButton> asdgasd</StyledNavButton>
+				<StyledNavUl>
+					<StyledNavLi href="#about" onClick={handleScroll}>
+						About
+					</StyledNavLi>
+					<StyledNavLi href="#myPortfolio" onClick={handleScroll}>
+						My Portfolio
+					</StyledNavLi>
+					<StyledNavLi href="#skills" onClick={handleScroll}>
+						Skills
+					</StyledNavLi>
+					<StyledNavLi href="#education" onClick={handleScroll}>
+						Education
+					</StyledNavLi>
+					<StyledNavLi href="#contact" onClick={handleScroll}>
+						Contact
+					</StyledNavLi>
+				</StyledNavUl>
 			</StyledNav>
 		</StyledNavBox>
 	);
