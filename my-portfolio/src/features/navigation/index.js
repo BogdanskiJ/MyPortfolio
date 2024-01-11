9116b0a000ade826d9590f104ad09900ece20cd3
Jakub Bogdanski <bogdanskikuba@gmail.com>
2024-01-11 16:27:33 +0100

    bubbleMoving added

--- my-portfolio/src/features/navigation/index.js
+++ my-portfolio/src/features/navigation/index.js
@@ -1,36 +1,103 @@
-import { StyledNav, StyledNavBox, StyledNavLi, StyledNavUl } from "./styled";
-
-const handleScroll = (e) => {
-	e.preventDefault();
-	const target = e.target.getAttribute("href");
-	const location = document.querySelector(target).offsetTop;
-
-	window.scrollTo({
-		left: 0,
-		top: location,
-		behavior: "smooth",
-	});
-};
+import {
+	StyledNav,
+	StyledNavBox,
+	StyledNavBubbleImg,
+	StyledNavLi,
+	StyledNavSpanBig,
+	StyledNavSpanMedium,
+	StyledNavSpanSmall,
+	StyledNavText,
+	StyledNavUl,
+} from "./styled";
+import Img1 from "./images/1.png";
+// import Img1 from "./images/2.png";
+// import Img1 from "./images/3.png";
 
 export const Navigation = () => {
+	const handleScroll = (e) => {
+		e.preventDefault();
+		const target = e.target.getAttribute("href");
+		const location = document.querySelector(target).offsetTop;
+
+		window.scrollTo({
+			left: 0,
+			top: location,
+			behavior: "smooth",
+		});
+	};
 	return (
 		<StyledNavBox>
 			<StyledNav>
 				<StyledNavUl>
-					<StyledNavLi href="#about" onClick={handleScroll}>
-						About
+					<StyledNavLi>
+						<StyledNavText href="#about" onClick={handleScroll}>
+							About
+						</StyledNavText>
+						<StyledNavSpanSmall>
+							<StyledNavBubbleImg src={Img1} />
+						</StyledNavSpanSmall>
+						<StyledNavSpanMedium>
+							<StyledNavBubbleImg src={Img1} />
+						</StyledNavSpanMedium>
+						<StyledNavSpanBig>
+							<StyledNavBubbleImg src={Img1} />
+						</StyledNavSpanBig>
 					</StyledNavLi>
-					<StyledNavLi href="#myPortfolio" onClick={handleScroll}>
-						My Portfolio
+					<StyledNavLi>
+						<StyledNavText href="#myPortfolio" onClick={handleScroll}>
+							My Portfolio
+						</StyledNavText>
+						<StyledNavSpanSmall>
+							<StyledNavBubbleImg src={Img1} />
+						</StyledNavSpanSmall>
+						<StyledNavSpanMedium>
+							<StyledNavBubbleImg src={Img1} />
+						</StyledNavSpanMedium>
+						<StyledNavSpanBig>
+							<StyledNavBubbleImg src={Img1} />
+						</StyledNavSpanBig>
 					</StyledNavLi>
-					<StyledNavLi href="#skills" onClick={handleScroll}>
-						Skills
+					<StyledNavLi>
+						<StyledNavText href="#skills" onClick={handleScroll}>
+							Skills
+						</StyledNavText>
+						<StyledNavSpanSmall>
+							<StyledNavBubbleImg src={Img1} />
+						</StyledNavSpanSmall>
+						<StyledNavSpanMedium>
+							<StyledNavBubbleImg src={Img1} />
+						</StyledNavSpanMedium>
+						<StyledNavSpanBig>
+							<StyledNavBubbleImg src={Img1} />
+						</StyledNavSpanBig>
 					</StyledNavLi>
-					<StyledNavLi href="#education" onClick={handleScroll}>
-						Education
+					<StyledNavLi>
+						<StyledNavText href="#education" onClick={handleScroll}>
+							Education
+						</StyledNavText>
+						<StyledNavSpanSmall>
+							<StyledNavBubbleImg src={Img1} />
+						</StyledNavSpanSmall>
+						<StyledNavSpanMedium>
+							<StyledNavBubbleImg src={Img1} />
+						</StyledNavSpanMedium>
+						<StyledNavSpanBig>
+							<StyledNavBubbleImg src={Img1} />
+						</StyledNavSpanBig>
 					</StyledNavLi>
-					<StyledNavLi href="#contact" onClick={handleScroll}>
-						Contact
+					<StyledNavLi>
+						<styledNavText href="#contact" onClick={handleScroll}>
+							Contact
+						</styledNavText>
+						<StyledNavSpanSmall>
+							<StyledNavBubbleImg src={Img1} />
+						</StyledNavSpanSmall>
+						<StyledNavSpanMedium>
+							<StyledNavBubbleImg src={Img1} />
+						</StyledNavSpanMedium>
+						<StyledNavSpanBig>
+							<StyledNavBubbleImg src={Img1} />
+						</StyledNavSpanBig>
 					</StyledNavLi>
 				</StyledNavUl>
 			</StyledNav>
--- my-portfolio/src/features/navigation/styled.js
+++ my-portfolio/src/features/navigation/styled.js
@@ -25,6 +25,183 @@ export const StyledNavUl = styled.ul`
 	font-size: 30px;
 `;
 
+export const StyledNavText = styled.span`
+	width: 100%;
+`;
+
+export const StyledNavSpanBig = styled.span`
+	display: block;
+	border-radius: 50%;
+	/* right: 0; */
+	right: 0;
+	z-index: 1;
+	bottom: 50%;
+	height: 130px;
+	width: 130px;
+	overflow: hidden;
+	position: absolute;
+	opacity: 0;
+	transform: scale(0);
+	transition: transform 0.5s ease;
+
+	@media ((max-width: ${({ theme }) => theme.breakpoint.xl})) {
+		height: 110px;
+		width: 110px;
+	}
+	@media ((max-width: ${({ theme }) => theme.breakpoint.lg})) {
+		height: 90px;
+		width: 90px;
+	}
+	@media ((max-width: ${({ theme }) => theme.breakpoint.md})) {
+		height: 75px;
+		width: 75px;
+	}
+	@media ((max-width: ${({ theme }) => theme.breakpoint.sm})) {
+		height: 60px;
+		width: 60px;
+	}
+
+	@keyframes bubblesAnimation {
+		0% {
+			opacity: 1;
+			bottom: 0;
+		}
+		90% {
+			opacity: 1;
+		}
+		100% {
+			opacity: 0;
+			bottom: 100%;
+		}
+	}
+
+	@keyframes sides {
+		0% {
+			margin-right: 0;
+		}
+		100% {
+			margin-right: 50px;
+		}
+	}
+`;
+export const StyledNavSpanMedium = styled.span`
+	display: block;
+	border-radius: 50%;
+	right: -20%;
+	z-index: 2;
+	bottom: -20%;
+	height: 90px;
+	width: 90px;
+	position: absolute;
+	overflow: hidden;
+	opacity: 0;
+	transform: scale(0);
+	transition: transform 0.5s ease;
+
+	@media ((max-width: ${({ theme }) => theme.breakpoint.xl})) {
+		height: 80px;
+		width: 80px;
+	}
+	@media ((max-width: ${({ theme }) => theme.breakpoint.lg})) {
+		height: 65px;
+		width: 65px;
+	}
+	@media ((max-width: ${({ theme }) => theme.breakpoint.md})) {
+		height: 50px;
+		width: 50px;
+	}
+	@media ((max-width: ${({ theme }) => theme.breakpoint.sm})) {
+		height: 30px;
+		width: 30px;
+	}
+
+	@keyframes bubblesAnimation {
+		0% {
+			opacity: 1;
+			bottom: 20%;
+		}
+		90% {
+			opacity: 1;
+		}
+		100% {
+			opacity: 0;
+			bottom: 100%;
+		}
+	}
+
+	@keyframes sidesMedium {
+		0% {
+			margin-right: 50px;
+		}
+		50% {
+			margin-right: 0;
+		}
+		100% {
+			margin-right: 50px;
+		}
+	}
+`;
+
+export const StyledNavSpanSmall = styled.span`
+	display: block;
+	border-radius: 50%;
+	width: 60px;
+	height: 60px;
+	right: -40%;
+	z-index: 3;
+	bottom: 0;
+	overflow: hidden;
+	position: absolute;
+	opacity: 0;
+	transform: scale(0);
+	transition: transform 0.5s ease;
+
+	@media ((max-width: ${({ theme }) => theme.breakpoint.xl})) {
+		height: 50px;
+		width: 50px;
+	}
+	@media ((max-width: ${({ theme }) => theme.breakpoint.lg})) {
+		height: 40px;
+		width: 40px;
+	}
+	@media ((max-width: ${({ theme }) => theme.breakpoint.md})) {
+		height: 30px;
+		width: 30px;
+	}
+	@media ((max-width: ${({ theme }) => theme.breakpoint.sm})) {
+		height: 20px;
+		width: 20px;
+	}
+
+	@keyframes bubblesAnimation {
+		0% {
+			opacity: 1;
+			bottom: 0;
+		}
+		90% {
+			opacity: 1;
+		}
+		100% {
+			opacity: 0;
+			bottom: 100%;
+		}
+	}
+
+	@keyframes sides {
+		0% {
+			margin-right: 0;
+		}
+		100% {
+			margin-right: 50px;
+		}
+	}
+`;
+
+export const StyledNavBubbleImg = styled.img`
+	width: 100%;
+	display: block;
+`;
+
 export const StyledNavLi = styled.li`
 	display: flex;
 	width: auto;
@@ -32,4 +209,31 @@ export const StyledNavLi = styled.li`
 	background: transparent;
 	border: none;
 	cursor: pointer;
+	position: relative;
+	text-transform: uppercase;
+	font-weight: bold;
+
+	&:hover {
+		${StyledNavSpanBig} {
+			transition-delay: 0s;
+			animation-delay: 0s;
+			transform: scale(1);
+			animation: bubblesAnimation 4s linear 1,
+				sides 2s ease-in-out infinite alternate;
+		}
+		${StyledNavSpanMedium} {
+			transition-delay: 0.5s;
+			animation-delay: 0.5s;
+			transform: scale(1);
+			animation: bubblesAnimation 4s linear 1,
+				sidesMedium 4s ease-in-out infinite;
+		}
+		${StyledNavSpanSmall} {
+			transition-delay: 1s;
+			animation-delay: 1s;
+			transform: scale(1);
+			animation: bubblesAnimation 4s linear 1,
+				sides 2s ease-in-out infinite alternate;
+		}
+	}
 `;
