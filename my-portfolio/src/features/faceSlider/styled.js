09931653b7422c0d365e03aa3d68309960d469a1
Jakub <bogdanskikuba@gmail.com>
2024-04-17 18:05:02 +0200

    change img

--- my-portfolio/src/features/faceSlider/styled.js
+++ my-portfolio/src/features/faceSlider/styled.js
@@ -3,32 +3,33 @@ import styled from "styled-components";
 import p1 from "../../images/projekt6.png";
 import p2 from "../../images/projekt8.png";
 import p3 from "../../images/projekt5.png";
+import p4 from "../../images/projekt5.webp";
 
 export const StyledImageBox = styled.div`
-	position: absolute;
-	top: 0;
-	left: 0;
-	width: 30%;
-	height: 100%;
-	z-index: 1;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 30%;
+  height: 100%;
+  z-index: 1;
 `;
 
 export const StyledImage3 = styled.div`
-	height: 100vh;
-	width: 100%;
-	background-position: top 5% left 6%;
-	background-repeat: no-repeat;
-	background-attachment: fixed;
-	background-size: 15%;
-	background-image: url(${p3});
+  height: 100vh;
+  width: 100%;
+  background-position: top 5% left 6%;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: 15%;
+  background-image: url(${p4});
 
-	@media screen and ((max-width: ${({ theme }) => theme.breakpoint.lg})) {
-		background-size: 18%;
-	}
-	@media screen and ((max-width: ${({ theme }) => theme.breakpoint.md})) {
-		background-size: 20%;
-	}
-	@media screen and ((max-width: ${({ theme }) => theme.breakpoint.sm})) {
-		background-size: 25%;
-	}
+  @media screen and ((max-width: ${({ theme }) => theme.breakpoint.lg})) {
+    background-size: 18%;
+  }
+  @media screen and ((max-width: ${({ theme }) => theme.breakpoint.md})) {
+    background-size: 20%;
+  }
+  @media screen and ((max-width: ${({ theme }) => theme.breakpoint.sm})) {
+    background-size: 25%;
+  }
 `;
